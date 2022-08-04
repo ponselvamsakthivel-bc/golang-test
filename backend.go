21fea8b1e6dc3a47e888c6ddf799b968a3f45827
root <root@ip-172-31-18-161.us-east-2.compute.internal>
2022-08-04 10:35:16 +0000

    initial commit

---
+++ backend.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"html"
+	"log"
+	"net/http"
+	"time"
+)
+
+type TimeData struct {
+	EpochTime int64  `json:"epoch_time"`
+	TimeZone  string `json:"time_zone"`
+}
+
+type JsonResponse struct {
+	Type string   `json:"type"`
+	Data TimeData `json:"data"`
+	// Message string `json:"message"`
+}
+
+func main() {
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello This is backend API which Uses Go-lang, %q", html.EscapeString(r.URL.Path))
+	})
+
+	http.HandleFunc("/epoch", func(w http.ResponseWriter, r *http.Request) {
+
+		var response = JsonResponse{Type: "success", Data: TimeData{EpochTime: currentEpochTime(), TimeZone: currentTimeZone()}}
+
+		json.NewEncoder(w).Encode(response)
+	})
+
+
+	fmt.Println("Server at 10000")
+	log.Fatal(http.ListenAndServe(":10000", nil))
+
+}
+
+func currentEpochTime() int64 {
+	return time.Now().Unix()
+}
+
+func currentTimeZone() string {
+	zone, _ := time.Now().Zone()
+	return zone
+}
+
